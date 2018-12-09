92926dad9ede7602cba693fd5d8c16678ea2f7de
Cicero Oliveira <cicero.oliveira@rsagroup.ca>
2018-12-09 01:15:27 -0500

    Carreers in Computer Science page redesigned using Skeleton

---
+++ code/careers-in-computer-science2.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<title>Gabriel Oliveira: ICS3UO</title>
+	<meta name="description" content="Gabriel Oliveira's webpage for the ICS3UO course.">
+	<meta name="author" content="Gabriel Oliveira">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="styles/Skeleton-2.0.4/css/normalize.css">
+  <link rel="stylesheet" href="styles/Skeleton-2.0.4/css/skeleton.css">
+	<!--link rel="stylesheet" href="styles/gabriels_stylesheet.css"-->
+
+	<style>
+		body {
+		  font-family: "Futura";
+		  background-color: #fff;
+		}
+
+		.home-button {
+		  background-color: #1e90ff;
+		  color: #ffffff;
+		  padding: 12px 16px;
+		  margin-left: 1em;
+		  font-size: 16px;
+		  cursor: pointer;
+		}
+
+		.container {
+			background-color: #f9fbfb;
+		}
+
+		.programs-container {
+			margin-top: 1em;
+		}
+
+		.programs-container h2 {
+			background-color: #124699;
+			color: #fff;
+			padding: 0.6em 0.6em 0.6em 0.6em;
+			text-align: center;
+		}
+
+		.header {
+			padding: 1em 1em 0 1em;
+			background-color: #ffb486;
+			color: #fff;
+			border: 1px solid #e76106;
+		}
+
+		.header h1 {
+		  border-bottom: 1px solid #fff;
+		}
+
+		.school-info {
+			background-color: #7397d0;
+			color: #fff;
+			padding: 1em;
+			margin-bottom: 1em;
+			border: 2px solid #124699;
+		}
+
+		.school-info h3, .school-info h4 {
+			border-bottom: 2px solid #fff;
+		}
+
+		.award {
+			font-weight: bold;
+			color: #0b3476;
+		}
+	</style>
+
+	<link rel="shortcut icon" href="images/logo.ico">
+</head>
+
+<body>
+	<nav>
+  	<a class="home-button" href="https://icsprogramming.ca/2018-2019/oliveiraff730/index.php"><i class="fa fa-home">&nbsp;Home</i></a>
+	</nav>
+
+	<div class="container">
+		<div class="row">
+			<div class="twelve columns header">
+	    	<h1>Careers in Computer Science</h1>
+
+				<h2>Information</h2>
+
+				<p class="information">This page displays many jobs that require computer programming skills and universities and colleges that offer computer programming programs. Also, schooling requirements for these programs and the many oppurtunities available for a career in computer programming are displayed on this page.</p>
+			</div>
+		</div>
+
+		<div class="row programs-container">
+			<div class="one-half column">
+
+				<h2>University Programs</h2>
+
+				<div class="school-info">
+					<h3>Computer Science at University of Toronto</h3>
+
+					<p>This program is a 4 year program that studies computer science at the University of Toronto. They look for notations for describing computations, and programming methodologies that facilitate the production of manageable and efficient software. In the theory of computation area, they study resource requirements in time and memory of many basic computational tasks.</p>
+
+					<h4>Requirements</h4>
+
+					<ul>
+						<li>Ontario Secondary School Diploma</li>
+						<li>Grade 12 English (ENG4U) credit</li>
+						<li>Grade 12 Calculus and Vectors (MCV4U) credit</li>
+						<li>Four additional Grade 12 university-level or university/college-level courses</li>
+						<li>(It is expected you would have completed Advanced Functions as a prerequisite to Calculus and Vectors)
+						<li>A grade average in at least the low 90s</li>
+					</ul>
+
+					<p class="award">This program will award you a Bachelor of Science degree.</p>
+				</div>
+
+				<div class="school-info">
+					<h3>Applied Computing at University of Windsor</h3>
+
+					<p>This program is a 4 year program that studies applied computing at the University of Windsor. It provides progressive, innovative tools and technology in computing, practical experience in developing software systems and the IT skills valued by industry.</p>
+
+					<h4>Requirements</h4>
+
+					<ul>
+						<li>Ontario Secondary School Diploma</li>
+						<li>Grade 12 English (ENG4U) credit</li>
+						<li>Grade 12 Advanced Functions (MHF4U) credit</li>
+						<li>Four additional Grade 12 university-level or university/college-level courses</li>
+						<li>Grade 12 Calculus and Vectors (MCV4U) is not required but is highly reccomended</li>
+						<li>A minimum average of 70% is required in math courses</li>
+						<li>A grade average in at least the low 70s</li>
+					</ul>
+
+					<p class="award">This program will award you a Bachelor of Computer Science degree.</p>
+				</div>
+
+				<div class="school-info">
+					<h3>Computer Science at Queens University</h3>
+
+					<p>This program is a 4 year program that studies computer science at Queens University. Computer Science, which is based on a firm foundation in logic and discrete mathematics, includes the study of computer hardware (the design of the machines themselves), computer software (the programs that cause the machines to perform useful tasks), and human-computer interaction (effective communication between people and their computing tools).</p>
+
+					<h4>Requirements</h4>
+
+					<ul>
+						<li>Ontario Secondary School Diploma</li>
+						<li>Grade 12 English (ENG4U) credit</li>
+						<li>Grade 12 Advanced Functions (MHF4U) credit</li>
+						<li>Grade 12 Calculus and Vectors (MCV4U) credit</li>
+						<li>Three additional Grade 12 university-level or university/college-level courses</li>
+						<li>A personal statement of experience</li>
+						<li>A grade average in at least the low 80s</li>
+					</ul>
+
+					<p class="award">This program will award you a Bachelor of Computing degree.</p>
+				</div>
+
+			</div>
+
+			<div class="one-half column">
+				<h2>College Programs</h2>
+
+				<div class="school-info">
+					<h3>Computer Programming at Humber College</h3>
+
+					<p>This program is a 2 year program that studies computer programming at Humber College. Your courses will include computer programming, systems analysis, web development, database management, information technology (IT), project management and other aspects of information systems. You will also learn the cycles of software development applications as follow the standards of planning, developing, testing and supporting.</p>
+
+					<h4>Requirements</h4>
+
+					<ul>
+						<li>Ontario Secondary School Diploma</li>
+						<li>Grade 12 English (ENG4U) or (ENG4C) credit</li>
+						<li>Grade 12 college-level or higher Math (MAP4C, MCT4C, MDM4U, MHF4U or MCV4U)</li>
+						<li>Two additional Grade 11 or 12 college-level or higher courses</li>
+						<li>Applicants who do not possess the required courses may complete admission testing to determine equivalencies</li>
+					</ul>
+
+					<p class="award">This program will award you a Diploma.</p>
+				</div>
+
+				<div class="school-info">
+					<h3>Computer Programming Analyst at George Brown College</h3>
+
+					<p>This program is a 3 year program that studies programming and analytics at George Brown College. During the first two years of the program you will develop the skills and techniques required for software application development and testing. The industry tells us they are looking for “the total package.” So the third year includes advanced technical skills in areas such as systems analysis and design techniques - and continues to develop communications, teamwork and other client service skills such as needs assessment, sales and presentation methods.</p>
+
+					<h4>Requirements</h4>
+
+					<ul>
+						<li>Ontario Secondary School Diploma</li>
+						<li>Grade 12 English (ENG4U) or (ENG4C) credit</li>
+						<li>One of the following courses: Grade 11 Functions and Applications (MCF3M), Grade 11 Functions (MCR3U), Grade 12 Foundations for College Mathematics (MAP4C), Grade 12 Mathematics for College Technology (MCT4C), Grade 12 Mathematics of Data Management (MDM4U), Grade 12 Advanced Functions (MHF4U) -or- Grade 12 Calculus and Vectors (MCV4U)</li>
+					</ul>
+
+					<p class="award">This program will award you an Advanced Diploma.</p>
+				</div>
+
+				<div class="school-info">
+					<h3>Software Development at Mohawk College</h3>
+
+					<p>Students in the Computer Systems Technology - Software Development program will experience a broad range of application development and computer systems. Experience a broad range of application development and computer systems Excel in: - Programming languages, database development - Web tools and mobile application development - System/business analysis Discover information technology including : - E-commerce - Project Management - Mobile Technologies</p>
+
+					<h4>Requirements</h4>
+
+					<ul>
+						<li>Ontario Secondary School Diploma</li>
+						<li>Grade 12 English (ENG4U) or (ENG4C) credit</li>
+						<li>Grade 12 college-level or higher Math (MAP4C, MCT4C, MDM4U, MHF4U or MCV4U)</li>
+						<li>A minimum grade of 60% is required in Grade 12 Foundations for College Mathematics (MAP4C)</li>
+						<li>A working knowledge in computers and desktop applications</li>
+					</ul>
+
+					<p class="award">This program will award you an Advanced Diploma.</p>
+				</div>
+			</div>
+	</div>
+</body>
+</html>
