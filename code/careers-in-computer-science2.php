964cde537524d236e742ee2a2391e7325cafb580
Gabriel Oliveira <goliveira@Gabriels-MacBook-Air.local>
2018-12-09 13:45:51 -0500

    careers-in-computer-science2.php

--- code/careers-in-computer-science2.php
+++ code/careers-in-computer-science2.php
@@ -41,6 +41,12 @@
 			padding: 0.6em 0.6em 0.6em 0.6em;
 			text-align: center;
 		}
+		.jobs-container h2 {
+			background-color: #b20000;
+			color: #fff;
+			padding: 0.6em 0.6em 0.6em 0.6em;
+			text-align: center;
+		}
 
 		.header {
 			padding: 1em 1em 0 1em;
@@ -55,6 +61,13 @@
 
 		.school-info {
 			background-color: #7397d0;
+			color: #fff;
+			padding: 1em;
+			margin-bottom: 1em;
+			border: 2px solid #124699;
+		}
+		.job-info {
+			background-color: #ff6666;
 			color: #fff;
 			padding: 1em;
 			margin-bottom: 1em;
@@ -77,7 +90,7 @@
 <body>
 	<nav>
   	<a class="home-button" href="https://icsprogramming.ca/2018-2019/oliveiraff730/index.php"><i class="fa fa-home">&nbsp;Home</i></a>
-	</nav>
+	</nav><br />
 
 	<div class="container">
 		<div class="row">
@@ -212,5 +225,85 @@
 				</div>
 			</div>
 	</div>
+	<div class="row jobs-container">
+		<div class="one-half column">
+
+			<h2>Jobs</h2>
+		<div class="job-info">
+			<h3>Computer Programmer</h3>
+			<p>Computer programmers write code to create software programs. This entails initially planning the software, creating models and flowcharts detailing how the code is to be written, writing and debugging code, and designing an application or systems interface.</p>
+			<h4>Information</h4>
+			<p>Computer programmers usually work in offices, most commonly in the computer systems design and related services industry. Programmers normally work alone, but sometimes work with other computer specialists on large projects. Because writing code can be done anywhere, many programmers telecommute.</p>
+			<h4>Skills</h4>
+			<ul>
+				<li><strong>Analytical skills.</strong> Computer programmers must understand complex instructions in order to create computer code.</li>
+				<li><strong>Concentration.</strong> Programmers must be able to work at a computer writing lines of code for long periods of time.</li>
+				<li><strong>Detail oriented.</strong> Computer programmers must closely examine the code they write because a small mistake can affect the entire computer program.</li>
+				<li><strong>Troubleshooting skills.</strong> An important part of a programmer’s job is to check the code for errors and fix any they find.</li>
+			</ul>
+			<h4>Requirements</h4>
+			<ul>
+				<li>UNIVERSITY EDUCATION REQUIRED</li>
+				<li>A bachelor's degree in computer science or in another discipline with a significant programming component or completion of a college program in computer science is usually required.</li>
+				<li>Specialization in programming for engineering and scientific applications requires specific post-secondary study or experience.</li>
+			</ul>
+			<h4>Outlook</h4>
+			<p>A computer programming job is in high demand in Ontario. The employment growth of computer programming is growing steadily, with about 7,000 new programmers every year.</p>
+			<p>Estimated salary: $75,000</p>
+			<p>Salary range: $40,000-$104,000</p>
+		</div>
+		<div class="job-info">
+			<h3>Web Developer</h3>
+			<p>Web developers design and create websites. When creating a website, developers have to make their client’s vision a reality. They work with clients to make sure it fits in with the type of site it is supposed to be, such as e-commerce, news, or gaming. Different types of websites may require different applications to work right. For example, a gaming site should be able to handle advanced graphics while an e-commerce site needs a payment processing application. The developer decides which applications and designs will best fit the site.</p>
+			<h4>Information</h4>
+			<p>Most web developers work full time. Web developers work in the following industries: computer systems design and related services, data processing, hosting, related services, and other information services, finance and insurance, educational services; provincial, local, and private, religious, civic, professional, and similar organizations.</p>
+			<h4>Skills</h4>
+			<ul>
+				<li><strong>Concentration.</strong> Web developers must sit at a computer and write detailed code for long periods.</li>
+				<li><strong>Creativity.</strong> Web developers are often involved in designing the appearance of a website and must make sure that it looks innovative and up to date.</li>
+				<li><strong>Customer-service skills.</strong> Web developers have to respond politely and correctly to user questions and requests.</li>
+				<li><strong>Detail Oriented.</strong> When web developers write in HTML, a minor error could cause an entire webpage to stop working.</li>
+			</ul>
+			<h4>Requirements</h4>
+			<ul>
+				<li>COLLEGE/APPRENTICESHIP EDUCATION REQUIRED</li>
+				<li>A bachelor's degree, usually in computer science, communications or business or completion of a college program in computer science, graphic arts, Web design or business is required.</li>
+				<li>Experience as a computer programmer or graphic designer is usually required.</li>
+			</ul>
+			<h4>Outlook</h4>
+			<p>A computer programming job is in moderate demand in Ontario. The employment growth of web development is growing steadily, with about 7,000 new developers every year.</p>
+			<p>Estimated salary: $59,000</p>
+			<p>Salary range: $31,000-$88,000</p>
+		</div>
+		<div class="job-info">
+			<h3>Computer Scientist</h3>
+			<p>
+Computer scientists create and improve computer algorithms, which are sets of instructions that tell a computer what to do. Some computer tasks are very difficult and require complex algorithms, however computer scientists try to simplify these algorithms to make computer systems as efficient as possible. These algorithms allow advancements in many types of technology, such as machine learning systems and cloud computing. The work of computer scientists often leads to technological advancements and efficiencies, such as better networking technology, faster computing speeds, and improved information security. In general, computer scientists work on a more theoretical level than do other computer professionals.</p>
+			<h4>Information</h4>
+			<p>Computer scientists improve ways to sort, manage, and display data. Most computer scientists employed by the federal government work for the Department of National Defence. Most computer scientists work full time. Those working on independent research may have flexible work schedules and work in office environments.</p>
+			<h4>Skills</h4>
+			<ul>
+				<li><strong>Analytical skills.</strong> Computer scientists must be organized in their thinking and analyze the results of their research to formulate conclusions.</li>
+				<li><strong>Communication skills.</strong> Computer scientists must communicate well with programmers and managers and be able to clearly explain their conclusions to people with no technical background. They often write for academic journals and similar publications.</li>
+				<li><strong>Critical-thinking skills.</strong> Computer scientists work on many complex problems.</li>
+				<li><strong>Detail oriented.</strong> Computer scientists must pay close attention to their work, because a small error can cause an entire project to fail.</li>
+				<li><strong>Ingenuity.</strong> Computer scientists must continually come up with innovative ways to solve problems, particularly when their ideas do not initially work as intended.</li>
+				<li><strong>Logical thinking.</strong> Computer algorithms rely on logic. Computer scientists must have a talent for reasoning.</li>
+				<li><strong>Math skills.</strong> Computer scientists must have knowledge of advanced math and other technical topics that are critical in computing.</li>
+			</ul>
+			<h4>Requirements</h4>
+			<ul>
+				<li>UNIVERSITY EDUCATION REQUIRED</li>
+				<li>A bachelor's or master's degree in computer science, business administration, commerce or engineering is usually required.</li>
+				<li>Several years of experience in systems analysis, data administration, software engineering, network design or computer programming, including supervisory experience, are required.</li>
+			</ul>
+			<h4>Outlook</h4>
+			<p>A computer programming job is in high demand in Ontario. The employment growth of computer science is growing steadily, with about 2,000 new computer scientists every year.</p>
+			<p>Estimated salary: $98,000</p>
+			<p>Salary range: $56,000-$140,000</p>
+		</div>
+	</div>
+</div>
+</div>
 </body>
 </html>
